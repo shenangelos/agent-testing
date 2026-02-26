4d77703ae5697d9d74c829a9ab536c13690db594
shenangelos <shenangelovicini@gmail.com>
2026-02-26 11:49:35 -0600

    AI: Added player movement script

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,21 +1,22 @@
 extends Node2D
 
-var speed = 5
+var speed = 200
 
 func _process(delta):
-    var movement = Vector2.ZERO
-    
-    if Input.is_action_pressed("ui_up"):
-        movement.y -= speed
-    if Input.is_action_pressed("ui_down"):
-        movement.y += speed
-    if Input.is_action_pressed("ui_left"):
-        movement.x -= speed
-    if Input.is_action_pressed("ui_right"):
-        movement.x += speed
-    
-    position += movement
+    var motion = Vector2.ZERO
 
-    # Keep the square within the game window boundaries
-    position.x = clamp(position.x, 0, get_viewport().size.x)
-    position.y = clamp(position.y, 0, get_viewport().size.y)
+    if Input.is_action_pressed("ui_up"):
+        motion.y -= 1
+    if Input.is_action_pressed("ui_down"):
+        motion.y += 1
+    if Input.is_action_pressed("ui_left"):
+        motion.x -= 1
+    if Input.is_action_pressed("ui_right"):
+        motion.x += 1
+
+    motion = motion.normalized() * speed * delta
+    position += motion
+
+    # Ensure the player square remains within the defined window boundaries
+    position.x = clamp(position.x, 0, get_viewport().size.x - self.scale.x * self.texture.get_size().x)
+    position.y = clamp(position.y, 0, get_viewport().size.y - self.scale.y * self.texture.get_size().y)
